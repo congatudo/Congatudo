Hypfer/Valetudo

refactor(ui): Reduce code duplication

frontend/src/valetudo/About.tsx
@@ -1,5 +1,5 @@
 import PaperContainer from "../components/PaperContainer";
-import {Box, Divider, Grid, Typography} from "@mui/material";
+import {Box, Grid} from "@mui/material";
 import {Info as AboutIcon} from "@mui/icons-material";
 import React from "react";
 import ReactMarkdown from "react-markdown";
@@ -8,23 +8,18 @@ import rehypeRaw from "rehype-raw";
 import style from "./About.module.css";
 import {AboutText} from "./res/AboutText";
 import {ReactComponent as Logo} from "../assets/icons/valetudo_logo_with_name.svg";
+import DetailPageHeaderRow from "../components/DetailPageHeaderRow";
 
 const About = (): JSX.Element => {
     return (
         <PaperContainer>
             <Grid container direction="row">
                 <Box style={{width: "100%"}}>
-                    <Grid item container alignItems="center" spacing={1} justifyContent="space-between">
-                        <Grid item style={{display:"flex"}}>
-                            <Grid item style={{paddingRight: "8px"}}>
-                                <AboutIcon/>
-                            </Grid>
-                            <Grid item>
-                                <Typography>About Valetudo</Typography>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Divider sx={{mt: 1}}/>
+                    <DetailPageHeaderRow
+                        title="About Valetudo"
+                        icon={<AboutIcon/>}
+                    />
+
                     <Grid
                         item
                         style={{

congatudo/Congatudo

frontend/src/valetudo/About.tsx
@@ -1,5 +1,5 @@
 import PaperContainer from "../components/PaperContainer";
-import {Box, Divider, Grid, Typography} from "@mui/material";
+import {Box, Grid} from "@mui/material";
 import {Info as AboutIcon} from "@mui/icons-material";
 import React from "react";
 import ReactMarkdown from "react-markdown";
@@ -8,23 +8,18 @@ import rehypeRaw from "rehype-raw";
 import style from "./About.module.css";
 import {AboutText} from "./res/AboutText";
 import {ReactComponent as Logo} from "../assets/icons/congatudo_logo_with_name.svg";
+import DetailPageHeaderRow from "../components/DetailPageHeaderRow";
 
 const About = (): JSX.Element => {
     return (
         <PaperContainer>
             <Grid container direction="row">
                 <Box style={{width: "100%"}}>
-                    <Grid item container alignItems="center" spacing={1} justifyContent="space-between">
-                        <Grid item style={{display:"flex"}}>
-                            <Grid item style={{paddingRight: "8px"}}>
-                                <AboutIcon/>
-                            </Grid>
-                            <Grid item>
-                                <Typography>About Congatudo</Typography>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Divider sx={{mt: 1}}/>
+                    <DetailPageHeaderRow
+                        title="About Congatudo"
+                        icon={<AboutIcon/>}
+                    />
+
                     <Grid
                         item
                         style={{